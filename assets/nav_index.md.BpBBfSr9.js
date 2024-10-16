a551c10929fb0da8af4d6755eeaf8b1f98a6f5a9
keli365 <keli365@foxmail.com>
2024-10-16 09:57:22 +0800

    Deploying to gh-pages from @ keli365/nav@af2e5c153be71ae4b46cec5fd26d25ac3042430a 🚀

---
+++ assets/nav_index.md.BpBBfSr9.js
@@ -0,0 +1,5 @@
+import{_ as a,c as n,o,m as e,a as t}from"./chunks/framework.sCaXEj8p.js";const v=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"nav/index.md","filePath":"nav/index.md","lastUpdated":1729043820000}'),s={name:"nav/index.md"},r=e("p",null,[t("export default defineConfig({ --- socialLinks: [{ icon: 'github', link: '"),e("a",{href:"https://github.com/maomao1996/vitepress-nav-template",target:"_blank",rel:"noreferrer"},"https://github.com/maomao1996/vitepress-nav-template"),t("' }], //社交链接")],-1),i=e("pre",null,[e("code",null,`footer: {
+  message: '如有转载或 CV 的请标注本站原文地址',
+  copyright: 'Copyright © 2019-present maomao'
+},  //页脚，可按Vue支持格式修改
+`)],-1),c=e("p",null,"})",-1),d=[r,i,c];function l(p,m,_,h,f,u){return o(),n("div",null,d)}const x=a(s,[["render",l]]);export{v as __pageData,x as default};
